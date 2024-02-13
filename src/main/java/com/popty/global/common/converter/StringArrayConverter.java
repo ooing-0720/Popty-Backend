43c0bb7c90ff0e6a62ee4a47babb26a9749f0c99
ooing-0720 <ooing-0720@naver.com>
2024-02-14 00:54:44 +0900

    FEAT DB converter 생성

---
+++ src/main/java/com/popty/global/common/converter/StringArrayConverter.java
@@ -0,0 +1,32 @@
+package com.popty.global.common.converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.List;
+
+@Converter
+public class StringArrayConverter implements AttributeConverter<List<String>, String> {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<String> dataList) {
+        try {
+            return mapper.writeValueAsString(dataList);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String dbData) {
+        try {
+            return mapper.readValue(dbData, List.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
